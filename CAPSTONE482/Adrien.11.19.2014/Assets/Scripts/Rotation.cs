9de6a27b7cfe18757dea8f3076165d46c68c61be
Jose Manriquez <josejlm2@neo.tamu.edu>
2014-11-24 13:01:13 -0600

    updating files

---
+++ CAPSTONE482/Adrien.11.19.2014/Assets/Scripts/Rotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	public float rotationSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (Vector3.up, rotationSpeed * Time.deltaTime);
+	}
+}
---
+++ TmVR Menu/Assets/Scripts/pickedupobj.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var refusethrow = false;
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function onTriggerEnter (other : Collider) {
+ if (other.gameObject.tag != "player" && other.gameObject.tag != "pickto"){ refusethrow = true;}
+ 
+}
+
+function onTriggerExit (other : Collider) {
+
+ if (other.gameObject.tag != "play" && other.gameObject.tag != "pickto") { refusethrow = false;}
+ 
+ }
+ 
